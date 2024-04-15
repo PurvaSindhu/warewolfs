e0e0893d310437c77480081646ce02642883506c
mounika yp <sindhujxworks@gmail.com>
2024-04-15 18:30:17 +0530

    conditional statements

---
+++ Wire.java
@@ -0,0 +1,26 @@
+class Wire{
+	
+	public static void main(String [] args){
+		
+		int numberOfTheBoxes=120;
+		String colourOfTheWire="black";
+		
+		if (numberOfTheBoxes==120 || colourOfTheWire=="black"){
+			System.out.println("the desired wire is selected");
+		}else {
+			System.out.println("the wire is not selected");
+		}
+		
+		if (numberOfTheBoxes==120 || colourOfTheWire=="red"){
+			System.out.println("the desired wire is selected");
+		}else {
+			System.out.println("the wire is not selected");
+		}
+		
+		if (numberOfTheBoxes==120 || colourOfTheWire=="purple"){
+			System.out.println("the desired wire is selected");
+		}else {
+			System.out.println("the wire is not selected");
+		}
+	}
+}
